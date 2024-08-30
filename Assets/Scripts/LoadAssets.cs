829d3861c18660427ad3ea41106b4ecc7be0a24b
g3-tou <munira.sangaaf@gmail.com>
2024-08-30 14:34:08 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
-    
+    public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+       Instantiate(redObj,new Vector3(2,0,0),Quaternion.identity);
+       Instantiate(blueObj,new Vector3(-2,0,0),Quaternion.identity); 
+
+        QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
